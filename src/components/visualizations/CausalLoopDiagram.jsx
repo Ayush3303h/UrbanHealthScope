b25840756ae23f9df1242008616a6dc46ada59b8
Ayush3303h <ayush.sahu2024@nst.rishihood.edu.in>
2025-05-07 22:12:47 +0530

    Update CausalLoopDiagram.jsx

--- src/components/visualizations/CausalLoopDiagram.jsx
+++ src/components/visualizations/CausalLoopDiagram.jsx
@@ -8,7 +8,7 @@ const CausalLoopDiagram = () => {
   // This is a simplified visual representation of a CLD, not a full interactive diagram
   
   return (
-    <img src='src/components/visualizations/cld.png'></img>
+    <img src="https://media-hosting.imagekit.io/8497837eecb24ae0/Screenshot%202025-05-06%20at%2010.46.03%E2%80%AFPM.png?Expires=1841244129&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=psvIWWrmzgTRszfrzdl1YqHLnNjQdQbYXcoFxAAW2HZHvWTBBeX2MrR7fLJkausiMz-c0cYP~6JneLghK40CNtjuw43IUtWCKTql8fCnXen8mh6FX2Q9mOZrCoy8IayS8nC3-7450GGILGlU4WoCRVzYAxieKeXa~pyNxbn53RnzqZAhzdmvvRSKV5KSW~3HaY1DH3Tyh8GcSFgMJx1tjNZp3UfoQd9F549Aqe06SpJ1mp5sz~k50~dF~rOz648rBjeUK-iuIkY9XNh1BbWCIO8QPbqMyWamE4xrt933Xf3DYj1DJzpz04ftsgLpb6VSOODXgTY8AhfR1j8Tkd9DFw__"></img>
   );
 };
 
